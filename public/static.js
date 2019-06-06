c394d393dda1ec203696e5b19c45bb32bae706ee
Matthew Lockdall <mlockdall26@gmail.com>
2019-06-05 23:46:48 -0500

    index page looking good

--- public/static.js
+++ public/static.js
@@ -1,18 +1,18 @@
 console.log('hello');
 
-const secToT = (secs) => {
-  let hr = Math.floor(secs / (60 * 60));
-  let divForMin = secs % (60 * 60);
-  let min = Math.floor(divForMin / 60);
-  let divForSec = divForMin % 60;
-  let seconds = Math.ceil(divForSec);
-  let time = hr + ':' + min + ':' + seconds
-  return time
-}
+// const secToT = (secs) => {
+//   let hr = Math.floor(secs / (60 * 60));
+//   let divForMin = secs % (60 * 60);
+//   let min = Math.floor(divForMin / 60);
+//   let divForSec = divForMin % 60;
+//   let seconds = Math.ceil(divForSec);
+//   let time = hr + ':' + min + ':' + seconds
+//   return time
+// }
 
 $(() => {
-  const $time = $('.yourTime').text()
-  $('.yourTime').text(secToT($time));
+  // const $time = $('.yourTime').text()
+  // $('.yourTime').text(secToT($time));
 
   console.log($('.indexTime'));
 
